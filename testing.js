07a4b71f93b501d0dc1126748950b3c3e8848c24
Theresa Lee <t10lee@ucsd.edu>
2014-04-22 22:07:32 -0700

    First commit. Basic nav bbar made.

---
+++ testing.js
@@ -0,0 +1,9 @@
+// var jumboHeight = $('#greeting').outerHeight();
+// function parallax(){
+//     var scrolled = $(window).scrollTop();
+//     $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+// }
+
+// $(window).scroll(function(e){
+//     parallax();
+// });
