666153b55f476935735eddb0e5fcb8e92e37c3e1
Theresa Lee <t10lee@ucsd.edu>
2014-04-23 13:42:48 -0700

    Added some JQuery to auto scroll to section by navbar

--- testing.js
+++ testing.js
@@ -7,3 +7,21 @@
 // $(window).scroll(function(e){
 //     parallax();
 // });
+
+ // This is a functions that scrolls to #{blah}link
+function goToByScroll(id){
+      // Remove "link" from the ID
+    //id = id.replace("link", "");
+      // Scroll
+    $('html,body').animate({
+        scrollTop: $(id).offset().top},
+        'slow');
+}
+
+$(".navbar-nav > li > a").click(function(e) { 
+      // Prevent a page reload when a link is pressed
+    e.preventDefault(); 
+      // Call the scroll function
+      console.log(this);
+    goToByScroll($(this).attr("href"));           
+});
