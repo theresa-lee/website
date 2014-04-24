f8a7a5a4fdd3a8a3715cc5c5459d5947b7d667c0
Theresa Lee <t10lee@ucsd.edu>
2014-04-24 01:11:01 -0700

    Functionality for nav bar. Added active

--- testing.js
+++ testing.js
@@ -9,19 +9,42 @@
 // });
 
  // This is a functions that scrolls to #{blah}link
-function goToByScroll(id){
-      // Remove "link" from the ID
-    //id = id.replace("link", "");
-      // Scroll
-    $('html,body').animate({
-        scrollTop: $(id).offset().top},
-        'slow');
-}
 
-$(".navbar-nav > li > a").click(function(e) { 
-      // Prevent a page reload when a link is pressed
-    e.preventDefault(); 
-      // Call the scroll function
-    goToByScroll($(this).attr("href"));           
+$( document ).ready(function() {
+
+	function goToByScroll(id){
+	      // Remove "link" from the ID
+	    //id = id.replace("link", "");
+	      // Scroll
+	    $('html,body').animate({
+	        scrollTop: $(id).offset().top},
+	        'slow');
+	}
+
+	$(".navbar-nav > li > a").click(function(e) { 
+	      // Prevent a page reload when a link is pressed
+	    e.preventDefault(); 
+	      // Call the scroll function
+	    goToByScroll($(this).attr("href"));           
+	});
+
+	function removeActives() {
+		$(".navbar-nav > li").each(function() {
+				$(this).removeClass("active");
+			});
+	}
+
+	$(".navbar-nav > li > a").click(function(e) {
+		e.preventDefault();
+		console.log(this);
+
+		if (!$(this).parent().hasClass("active")) {
+
+			removeActives();
+
+			$(this).parent().addClass("active");
+			console.log(this);
+		}
+
+	});
 });
-
